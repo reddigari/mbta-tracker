cbd487190d5aba091835871ec42e1c320a177e77
Samir Reddigari <samirreddigari@gmail.com>
2018-05-27 22:22:16 -0400

    Initial commit

---
+++ src/components/Route.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+const style = {
+    color: 'white',
+}
+
+export class Route extends Component {
+    render() {
+        const name = this.props.route.long_name || this.props.route.short_name;
+        const color = "#" + this.props.route.color;
+        return (
+            <button onClick={this.props.onClick}
+                    style={{...style, backgroundColor: color}}
+                    className="btn btnRoute mx-1 my-1">
+                {name}
+            </button>
+        )
+    }
+}
+
+export class RouteNav extends Component {
+    render() {
+        if (this.props.routes.length === 0) {
+            return ( <div /> )
+        }
+
+        return (
+            <div className="row justify-content-center">
+                {this.props.routes.map((route) => (
+                    <Route key={route.id} 
+                           route={route.attributes}
+                           routeId={route.id}
+                           onClick={() => this.props.onClick(route.id)} />
+                ))}
+            </div>
+        )
+    }
+}
